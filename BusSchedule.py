82b30e3d33a4cbce5a0f09057feaf4921dad48ad
kmoswald <kaitlynoswald@unomaha.edu>
2025-10-21 05:02:01 +0000

    Started

--- BusSchedule.py
+++ BusSchedule.py
@@ -39,18 +39,52 @@ def loadTestPage():
   return contents
 
 def getHours(time):
+  now = datetime.datetime.now()
+  currentHour = (now.hour - 5) % 24
+  currentMinute = now.minute
 
-  Take a time in the format "HH:MM AM and return hour in 24-hour format"
+  print (currentHour, currentMinute)
+  #if hour 
+
+  #hour = hour + 12
+
+
+  "Take a time in the format HH:MM AM and return hour in 24-hour format"
 
   return 21
+
+#def getMinutes(time):
+#Given a string in the form "HH:MM AM/PM" will return just the minutes portion.
+
+
+#def isLater(time1, time2):
+#Provide True or False if time 1 is later than time 2
+#hour1 = getHours(time1)
+#minutes1 = getMinutes(time1)
+#hour2 = getHours(time2)
+#minutes2 = getMinutes(time2)
+#if hour1 > hour2
+  #return True
+#elif hour1 < hour2
+  #return False
+#else
+  #if minute1 > minute2:
+    #return True
+   #else
+    #return False 
 
 def main():
   direction = "EAST"
 
-  url = "https://myride.ometro.com/Schedule?stopCode=2269&routeNumber=11&directionName=" + direction
-  #c1 = loadURL(url) #loads the web page
+  #url = "https://myride.ometro.com/Schedule?stopCode=2269&routeNumber=11&directionName=" + direction
+  url = "https://myride.ometro.com/Schedule?stopCode=3060&date=2025-10-20&routeNumber=18&directionName="+ direction
+  c1 = loadURL(url) #loads the web page
   #c1 = loadTestPage() #loads the test page
-  #print(c1)
-  time = "9:49 AM"
+  print(c1)
+  time = "9:49 PM"
 
+  #print("Current time:" + )
+  #print("The next bus will arrive in" +)
+  #print("The following bus will arrive in " + )
+  
 main()
